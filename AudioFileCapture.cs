2f6a390f1f2212dfe39ed382328c3cf221adde35
beejay.urzo <beejay.urzo@gmail.com>
2023-06-15 21:54:56 +0800

    Add project files.

---
+++ AudioFileCapture.cs
@@ -0,0 +1,243 @@
+﻿using OpenTok;
+using System;
+using System.Collections;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using NAudio;
+using NAudio.Wave;
+using System.Xml.Linq;
+using System.IO;
+
+namespace AudioCaptureFromFile
+{
+    internal class AudioFileCapture : IAudioDevice
+    {
+        bool renderInitialized = false;
+        bool renderStarted = false;
+        bool captureInitialized = false;
+        bool captureStarted = false;        
+        int numberOfChannels = 1;
+        int sampleRate = 48000;
+        private AudioDevice.AudioBus audioBus;
+        Queue audioDataQueue = new Queue();
+        
+
+        public void LoadFileToBeSent(String audiofile)
+        {
+            byte[] data;
+            Debug.WriteLine("start reading audio romfile");
+            try
+            {
+                using (var reader = new AudioFileReader(audiofile))
+                {
+                    var outFormat = new WaveFormat(sampleRate, numberOfChannels);
+                    using (var conversionStream = new MediaFoundationResampler(reader, outFormat))
+                    {
+                        //Start reading PCM data
+                        using (MemoryStream wavData = new MemoryStream())
+                        {
+                            var ByteCount = 0;
+                            var readBuffer = new byte[1024];
+                            while ((ByteCount = conversionStream.Read(readBuffer, 0, readBuffer.Length)) != 0)
+                            {
+                                wavData.Write(readBuffer, 0, ByteCount);
+                            }
+                            data = wavData.ToArray();
+                        }
+                    }
+                }
+                Debug.WriteLine($"Enqueuing data for OpenTok - Started, Data size:{data.Length}");
+                int bufferSize = sampleRate * numberOfChannels * 2 / 100; // Number of bytes in a 10ms batch
+                int index = 0;
+                while (index < data.Length)
+                {
+                    int remainingData = data.Length - index;
+                    int chunkSize = Math.Min(bufferSize, remainingData);
+                    byte[] chunk = new byte[chunkSize];
+                    
+                    Array.Copy(data, index, chunk, 0, chunkSize);
+                    audioDataQueue.Enqueue(chunk);
+                    
+                    index += chunkSize;
+                }
+
+                Debug.WriteLine("Enqueuing data for OpenTok - Finished");
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"{ex} An error occurred while enqueuing data for OpenTok: {ex.Message}");
+            }
+        }
+
+
+
+        private void SendToAudioBus(byte[] buffer)
+        {
+            if (audioBus == null)
+            {
+                Debug.WriteLine("AUDIO BUS NULL");
+                return;
+            }
+                
+            
+            int bufferSize = buffer.Length;
+            int count = (buffer.Length / 2) / numberOfChannels;
+
+            IntPtr pointer = Marshal.AllocHGlobal(bufferSize);
+
+            Marshal.Copy(buffer, 0, pointer, bufferSize);
+            audioBus.WriteCaptureData(pointer, count);
+            Marshal.FreeHGlobal(pointer);
+
+            Debug.WriteLine($"Data written to audio bus: {bufferSize}");
+        }
+
+        public AudioFileCapture()
+        {
+
+        }
+
+        public void DestroyAudio()
+        {
+            Console.WriteLine("Destroying Audio");
+            DestroyAudioCapturer();
+            DestroyAudioRenderer();
+            Console.WriteLine("Audio Destroyed");
+        }
+
+        public void DestroyAudioCapturer()
+        {
+            StopAudioCapturer();
+        }
+
+        public void DestroyAudioRenderer()
+        {
+            StopAudioRenderer();
+        }
+
+        public AudioDeviceSettings GetAudioCapturerSettings()
+        {
+            Console.WriteLine("Creating new audio capturer settings");
+            AudioDeviceSettings capturerSettings = new AudioDeviceSettings();
+            capturerSettings.NumChannels = numberOfChannels;
+            capturerSettings.SamplingRate = sampleRate;
+
+            Console.WriteLine($"Capturer Sampling Rate: {capturerSettings.SamplingRate} - {capturerSettings.NumChannels} ch");
+            return capturerSettings;
+        }
+
+        public AudioDeviceSettings GetAudioRendererSettings()
+        {
+
+            Console.WriteLine("Creating new audio renderer settings");
+            AudioDeviceSettings rendererSettings = new AudioDeviceSettings();
+            rendererSettings.NumChannels = numberOfChannels;
+            rendererSettings.SamplingRate = sampleRate;
+
+            Console.WriteLine($"Renderer Sampling Rate: {rendererSettings.SamplingRate} - {rendererSettings.NumChannels} ch");
+            return rendererSettings;
+        }
+
+        public int GetEstimatedAudioCaptureDelay()
+        {
+            return 0;
+        }
+
+        public int GetEstimatedAudioRenderDelay()
+        {
+            return 0;
+        }
+
+        public void InitAudio(AudioDevice.AudioBus audioBus)
+        {
+            Debug.WriteLine("Audio Initiated");
+            this.audioBus = audioBus;
+        }
+
+        public void InitAudioCapturer()
+        {
+            if (captureInitialized)
+                return;
+            captureInitialized = true;
+        }
+
+        public void InitAudioRenderer()
+        {
+            renderInitialized = true;
+        }
+
+        public bool IsAudioCapturerInitialized()
+        {
+            Console.WriteLine($"Checking Audio Capturer Initialized [{captureInitialized}]");
+            return captureInitialized;
+        }
+
+        public bool IsAudioCapturerStarted()
+        {
+            Console.WriteLine($"Checking Audio Capturer Started [{captureStarted}]");
+            return captureStarted;
+        }
+
+        public bool IsAudioRendererInitialized()
+        {
+            return renderInitialized;
+        }
+
+        public bool IsAudioRendererStarted()
+        {
+            return renderStarted;
+        }
+
+        public void StartAudioCapturer()
+        {
+            if (captureStarted)
+                return;
+            captureStarted = true;
+            Task.Run(() =>
+            {
+                DateTime nextBatchTime = DateTime.Now;
+                while (captureStarted)
+                {
+                    try
+                    {
+                        if (DateTime.Now >= nextBatchTime)
+                        {
+                            nextBatchTime += new TimeSpan(0, 0, 0, 0, 10);
+
+                            
+
+                            if (audioDataQueue.Count > 0)
+                            {
+                                byte[] b = (byte[])audioDataQueue.Dequeue();
+                                SendToAudioBus(b);
+                            }
+                               
+                        }
+                        System.Threading.Thread.Sleep(1);
+                    }
+                    catch (Exception ex)
+                    {
+                    }
+                }
+            });
+        }
+
+        public void StartAudioRenderer()
+        {
+            renderStarted = true;
+        }
+
+        public void StopAudioCapturer()
+        {
+            captureStarted = false;
+        }
+
+        public void StopAudioRenderer()
+        {
+            renderStarted = false;
+        }
+    }
+}
+
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Windows;
+using Microsoft.SqlServer.Server;
+using NAudio.Wave;
+using System.Windows.Markup;
+using OpenTok;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text;
+using System.ComponentModel;
+
+namespace AudioCaptureFromFile
+{
+    public partial class MainWindow : Window
+    {
+        private const string API_KEY = "47464991";
+        private const string SESSION_ID = "2_MX40NzQ2NDk5MX5-MTY4NjgzMzk5ODMxOH5LUlc3c1h6VkNXemN6Z2RaeVNGUkZNMmV-fn4";
+        private const string TOKEN = "T1==cGFydG5lcl9pZD00NzQ2NDk5MSZzaWc9MTRmZDcwOGVhNzJhODE1ODhhMmQ1NTQwMTQwMjJlN2I3NTNkMzUyODpzZXNzaW9uX2lkPTJfTVg0ME56UTJORGs1TVg1LU1UWTROamd6TXprNU9ETXhPSDVMVWxjM2MxaDZWa05YZW1ONloyUmFlVk5HVWtaTk1tVi1mbjQmY3JlYXRlX3RpbWU9MTY4NjgzNDAyMyZub25jZT0wLjE2NzMwOTQ3MjUxNTc5OTUzJnJvbGU9cHVibGlzaGVyJmV4cGlyZV90aW1lPTE2ODY5MjA0MjMmaW5pdGlhbF9sYXlvdXRfY2xhc3NfbGlzdD0=";
+        Context context = new Context(new WPFDispatcher());
+        
+        private Session Session;
+        private Publisher Publisher;
+        private AudioFileCapture AudioFileRd ;
+
+        StringBuilder sb = new StringBuilder();
+        private String logfile = "../../log.txt";
+
+        //the audio file you want to load
+        //private const string audiofile = "../../sample-48khz-96kbit.mp3";
+        //private const string audiofile = "../../sample-48khz-96kbit.wav";
+        //private const string audiofile = "../../sample-24khz-48kbit.mp3";
+        private const string audiofile = "../../sample-24khz-48kbit.wav";
+
+        public object Data { get; private set; }
+
+        public MainWindow()
+        {
+            Debug.WriteLine("Starting");
+
+            void OTlogger(string message)
+            {
+                sb.AppendLine(message);
+            }
+            OpenTok.Logger.LogCallback OTLog = OTlogger;
+
+            OpenTok.Logger.Enable(Logger.Level.Debug, true, OTLog);
+            InitializeComponent();
+       
+            
+            AudioFileRd = new AudioFileCapture();
+
+            AudioFileRd.LoadFileToBeSent(audiofile);
+            AudioDevice.SetCustomAudioDevice(context, AudioFileRd);
+
+            IList<VideoCapturer.VideoDevice> capturerDevices = VideoCapturer.EnumerateDevices();
+            if (capturerDevices == null || capturerDevices.Count == 0)
+                throw new Exception("No video capture devices detected");
+            
+            Publisher = new Publisher.Builder(context)
+            {
+                Capturer = capturerDevices[0].CreateVideoCapturer(VideoCapturer.Resolution.High, VideoCapturer.FrameRate.Fps30),
+                Renderer = PublisherVideo,
+                HasAudioTrack = true
+
+            }.Build();
+            
+            Session = new Session.Builder(context, API_KEY, SESSION_ID).Build();
+            Session.Connected += Session_Connected;
+            Session.Disconnected += Session_Disconnected;
+            Session.Error += Session_Error;
+            Session.StreamReceived += Session_StreamReceived;
+            
+            Session.Connect(TOKEN);           
+        }
+
+        public byte[] AudioReader(string filename, int samplerate, int bits, int channel)
+        {
+            Data = null;
+            Debug.WriteLine("Reading Data");
+            WaveFormat Format = new WaveFormat(samplerate, bits, channel);
+            using (WaveFileReader reader = new WaveFileReader(filename))
+            {
+                byte[] src = new byte[reader.Length];
+                reader.Read(src, 0, src.Length);
+                return src;
+            }            
+        }
+
+        private  void Session_Connected(object sender, System.EventArgs e)
+        {
+            Session.Publish(Publisher);
+            Debug.WriteLine("Publishing");
+        }
+ 
+        private void Session_Disconnected(object sender, System.EventArgs e)
+        {
+            Trace.WriteLine("Session disconnected.");
+        }
+
+        private void Session_Error(object sender, Session.ErrorEventArgs e)
+        {
+            Trace.WriteLine("Session error:" + e.ErrorCode);
+        }
+
+        private void Session_StreamReceived(object sender, Session.StreamEventArgs e)
+        {
+            Subscriber subscriber = new Subscriber.Builder(context, e.Stream)
+            {
+                Renderer = SubscriberVideo
+            }.Build();
+            Session.Subscribe(subscriber);
+        }
+
+        void MainWindow_Closing(object sender, CancelEventArgs e)
+        {
+            MessageBox.Show("Closing called");
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(logfile))
+            {
+                file.WriteLine(sb.ToString()); // "sb" is the StringBuilder
+
+            }
+           
+        }
+
+    }
+}
